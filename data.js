1250f75727eb6815285617db1ff1b17b1ea99d8f
ryanwlight <48245284+ryanwlight@users.noreply.github.com>
2024-04-23 05:42:16 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const cal = new CalHeatmap();
+
+cal.paint({
+  itemSelector: "#ex-year",
+  domain: {
+    type: "month",
+    gutter: 10,
+  },
+  subDomain: {
+    type: "day",
+    radius: 2,
+  },
+  date: {
+    start: '2024-01-01',
+  },
+  data: {
+    source: 'https://raw.githubusercontent.com/wa0x6e/cal-heatmap/gh-pages/fixtures/seattle-weather.csv',
+    requestInit: {
+      mode: 'no-cors',
+    },
+    type: 'csv',
+    x: 'date',
+    y: d => +d['temp_max'],
+    groupY: 'max',
+  },
+  scale: { color: { type: 'linear', scheme: 'PRGn', domain: [0, 40] } },
+});
