afb994556b3c7cb2c5d56bb06b3b829f9f7401fa
ryanwlight <48245284+ryanwlight@users.noreply.github.com>
2024-04-23 08:56:39 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,37 +1,8 @@
 const weekDaysTemplate = DateHelper => ({
   name: 'weekday',
   parent: 'day',
-  rowsCount: () => 5,
+  rowsCount: () => 10,
   columnsCount: () => 54,
-  mapping: (startTimestamp, endTimestamp) => {
-    let weekNumber = 0;
-    let x = -1;
-
-    return DateHelper.intervals(
-      'day',
-      startTimestamp,
-      DateHelper.date(endTimestamp)
-    )
-      .map(ts => {
-        const date = DateHelper.date(ts);
-
-        if (weekNumber !== date.week()) {
-          weekNumber = date.week();
-          x += 1;
-        }
-
-        if (date.format('d') === '0' || date.format('d') === '6') {
-          return null;
-        }
-
-        return {
-          t: ts,
-          x,
-          y: date.format('d') - 1,
-        };
-      })
-      .filter(n => n !== null);
-  },
 });
 
 
