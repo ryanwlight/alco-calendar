149714d0e761005489e8ee2fba6b1a6d860570d1
ryanwlight <48245284+ryanwlight@users.noreply.github.com>
2024-04-23 08:43:12 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,52 @@
+const weekDaysTemplate = DateHelper => ({
+  name: 'weekday',
+  parent: 'day',
+  rowsCount: () => 5,
+  columnsCount: () => 54,
+  mapping: (startTimestamp, endTimestamp) => {
+    let weekNumber = 0;
+    let x = -1;
+
+    return DateHelper.intervals(
+      'day',
+      startTimestamp,
+      DateHelper.date(endTimestamp)
+    )
+      .map(ts => {
+        const date = DateHelper.date(ts);
+
+        if (weekNumber !== date.week()) {
+          weekNumber = date.week();
+          x += 1;
+        }
+
+        if (date.format('d') === '0' || date.format('d') === '6') {
+          return null;
+        }
+
+        return {
+          t: ts,
+          x,
+          y: date.format('d') - 1,
+        };
+      })
+      .filter(n => n !== null);
+  },
+});
+
+
 const cal = new CalHeatmap();
+cal.addTemplates(weekDaysTemplate);
 
 cal.paint({
   itemSelector: "#ex-year",
   range: 48,
   domain: {
-    type: "month",
+    type: "year",
     gutter: 10,
   },
   subDomain: {
-    type: "day",
+    type: "weekday",
     radius: 2,
   },
   date: {
